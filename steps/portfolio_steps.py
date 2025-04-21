06fe090089d42d5a03aef48f002a680306ba13ee
Mairon Hernandez <mairon.hernandez@gmail.com>
2025-04-21 21:36:33 +0800

    Fix: Fixed the waiting time to shorten the waiting

--- steps/portfolio_steps.py
+++ steps/portfolio_steps.py
@@ -7,15 +7,14 @@ import time
 def step_impl(context, url: str):
     context.page.goto(url)
 
-@when(u'I logged in using my username "{username}" and password "{password}"')
+@when(u'I login using my username "{username}" and password "{password}"')
 def step_impl(context, username: str, password: str):
     context.page.fill('#user-name', username)
-    time.sleep(3)
     context.page.fill('#password', password)
-    time.sleep(3)
+    time.sleep(1)
     context.page.click('#login-button')
 
 @then(u'the page title should contain "{expected_title}"')
 def step_impl(context, expected_title: str):
-    time.sleep(10)
+    time.sleep(3)
     assert expected_title in context.page.title()
